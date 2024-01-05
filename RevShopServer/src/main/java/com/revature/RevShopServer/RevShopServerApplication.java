ad458448c40212612dd183fd49e6c51f6750ab51
DESKTOP-CBTULA3\Roman <magiccoat2000@gmail.com>
2024-01-05 10:11:57 -0500

    SpringBootPush

---
+++ RevShopServer/src/main/java/com/revature/RevShopServer/RevShopServerApplication.java
@@ -0,0 +1,13 @@
+package com.revature.RevShopServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevShopServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevShopServerApplication.class, args);
+	}
+
+}
---
+++ RevShopServer/src/test/java/com/revature/RevShopServer/RevShopServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.RevShopServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevShopServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
