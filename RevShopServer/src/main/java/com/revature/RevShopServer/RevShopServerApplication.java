ce8909c9a2f033d0ae1b2d2d72510df2cd17ce6e
ashertukde <Shertukde@gmail.com>
2024-01-08 12:45:35 -0800

    worked on setting up the front end for registering

--- RevShopServer/src/main/java/com/revature/RevShopServer/RevShopServerApplication.java
+++ RevShopServer/src/main/java/com/revature/RevShopServer/RevShopServerApplication.java
@@ -2,8 +2,14 @@ package com.revature.RevShopServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = {"com.revature.RevShopServer.entities"})
+@SpringBootApplication(scanBasePackages = {"com.revature.RevShopServer.entities",
+		"com.revature.RevShopServer.controllers",
+		"com.revature.RevShopServer.config",
+		"com.revature.RevShopServer.services",
+		"com.revature.RevShopServer.repositories"},
+		exclude = { SecurityAutoConfiguration.class })
 public class RevShopServerApplication {
 
 	public static void main(String[] args) {
