1eec8ee9d282645c0d99d417bbe01699216b7994
DESKTOP-CBTULA3\Roman <magiccoat2000@gmail.com>
2024-01-08 14:11:45 -0500

    emails setup

--- RevShopServer/src/main/java/com/revature/RevShopServer/RevShopServerApplication.java
+++ RevShopServer/src/main/java/com/revature/RevShopServer/RevShopServerApplication.java
@@ -1,13 +1,18 @@
 package com.revature.RevShopServer;
 
+import com.revature.RevShopServer.services.MailSenderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
-@SpringBootApplication(scanBasePackages = {"com.revature.RevShopServer.entities"})
+@SpringBootApplication(scanBasePackages = {"com.revature.RevShopServer.entities", "com.revature.RevShopServer.services"} )
 public class RevShopServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RevShopServerApplication.class, args);
 	}
+
 
 }
