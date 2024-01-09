7b28041c0b4d3ef52596160e96e62ebec56b3a3a
ashertukde <Shertukde@gmail.com>
2024-01-09 08:21:08 -0800

    finished registering a user

--- RevShopServer/src/main/java/com/revature/RevShopServer/entities/Buyer.java
+++ RevShopServer/src/main/java/com/revature/RevShopServer/entities/Buyer.java
@@ -25,6 +25,10 @@ public class Buyer {
         this.account = account;
     }
 
+    public Buyer(Account account) {
+        this.account = account;
+    }
+
     public Integer getBuyerId() {
         return buyerId;
     }
