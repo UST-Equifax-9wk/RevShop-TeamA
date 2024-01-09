bdf23734ff5b17f0abd4f035725b7075e60bedd9
DESKTOP-CBTULA3\Roman <magiccoat2000@gmail.com>
2024-01-09 16:27:11 -0500

    discounts

---
+++ RevShopClient/src/app/register-discount/register-discount.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-discount',
+  standalone: true,
+  imports: [],
+  templateUrl: './register-discount.component.html',
+  styleUrl: './register-discount.component.css'
+})
+export class RegisterDiscountComponent {
+
+}
