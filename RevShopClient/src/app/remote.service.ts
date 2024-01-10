82d34e6ae0f51b37899983957343498091d681ab
ashertukde <Shertukde@gmail.com>
2024-01-10 13:39:53 -0800

    restoring remote service

--- RevShopClient/src/app/remote.service.ts
+++ RevShopClient/src/app/remote.service.ts
@@ -1,79 +1,113 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
-import { Observable , catchError} from 'rxjs';
+import { Observable, catchError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RemoteService {
-
   httpClient: HttpClient;
   baseUrl: String;
-  httpOptions ={
+  httpOptions = {
     observe: 'response',
-    withCredentials:true,
-    headers: new HttpHeaders({'Content-Type':'application/json'})
-  }
+    withCredentials: true,
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
   constructor(httpClient: HttpClient) {
     this.httpClient = httpClient;
-    this.baseUrl = "http://localhost:8080"
+    this.baseUrl = 'http://localhost:8080';
   }
-  sendEmail(message:NewMessageDto){
-    return this.httpClient.post(this.baseUrl + "/sendEmail", JSON.stringify(message),{
+  sendEmail(message: NewMessageDto) {
+    return this.httpClient.post(
+      this.baseUrl + '/sendEmail',
+      JSON.stringify(message),
+      {
+        observe: 'response',
+        withCredentials: true,
+        headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      }
+    );
+  }
+  registerNewProduct(product: NewProductDto, sellerId: number) {
+    return this.httpClient.post(
+      this.baseUrl + '/Products/' + sellerId + '/addProduct',
+      JSON.stringify(product),
+      {
+        observe: 'response',
+        withCredentials: true,
+        headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      }
+    );
+  }
+  getAllProducts() {
+    return this.httpClient.get(this.baseUrl + '/Products/getProducts', {
       observe: 'response',
-      withCredentials: true ,
-      headers: new HttpHeaders({'Content-Type': 'application/json'})
-    })
+      withCredentials: true,
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+    });
   }
-  registerNewProduct(product:NewProductDto, sellerId:number){
-    return this.httpClient.post(this.baseUrl + "/Products/" + sellerId + "/registerProduct", JSON.stringify(product),{
-      observe: 'response',
-      withCredentials: true ,
-      headers: new HttpHeaders({'Content-Type': 'application/json'})
-    })
+  saveUser(user: AccountDto) {
+    return this.httpClient.post(
+      this.baseUrl + '/register',
+      JSON.stringify(user),
+      {
+        observe: 'response',
+        withCredentials: true,
+        headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      }
+    );
   }
-
-
-  saveUser(user:AccountDto){
-    return this.httpClient.post(this.baseUrl + "/register",JSON.stringify(user),{
-      observe: 'response',
-      withCredentials: true ,
-      headers: new HttpHeaders({'Content-Type': 'application/json'})
-    })
+  saveOrder(order: OrderDto) {
+    return this.httpClient.post(
+      this.baseUrl + '/setorder',
+      JSON.stringify(order),
+      {
+        observe: 'response',
+        withCredentials: true,
+        headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+      }
+    );
   }
 }
-export interface NewMessageDto{
-  recipient:string
-  subject:string
-  message:string
+export interface NewMessageDto {
+  recipient: string;
+  subject: string;
+  message: string;
 }
-export interface NewProductDto{
-  name:string
-  description:string
-  category:string
-  price:number
-  inventoryCount:number
+export interface NewProductDto {
+  name: string;
+  description: string;
+  category: string;
+  price: number;
+  inventoryCount: number;
+  seller?:SellerDto;
 }
-export interface AccountDto{
-  username:string
-  password:string
-  email:string
-  phone:string
-  accountType:string
+export interface SellerDto{
+  account:AccountDto;
+  sellerId:number;
 }
-export interface BuyerDto{
-  firstname:string
-  lastname:string
-  account:AccountDto
+export interface AccountDto {
+  username: string;
+  password: string;
+  email: string;
+  phone: string;
+  accountType: string;
 }
-export interface OrderDto{
-  shippingAddress:string
-  billingAddress:string
-  timestamp:string
-  buyer?:BuyerDto
+export interface BuyerDto {
+  firstname: string;
+  lastname: string;
+  account: AccountDto;
 }
-export interface OrderItemDto{
-  order:OrderDto
-  amount:number
-  price:number
+export interface OrderDto {
+  orderId?: string;
+  shippingAddress: string;
+  billingAddress: string;
+  timestamp?: string;
+  buyer?: BuyerDto;
+  orderStatus: string;
+}
+export interface OrderItemDto {
+  order: OrderDto;
+  amount: number;
+  price: number;
 }
