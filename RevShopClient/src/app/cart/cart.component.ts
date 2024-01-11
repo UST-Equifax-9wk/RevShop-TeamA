ee53fa7e1a1bd4ac69f85ea4efa82c72c2ef9683
Cyaorn <diepc153@gmail.com>
2024-01-11 11:57:46 -0600

    cart page works

--- RevShopClient/src/app/cart/cart.component.ts
+++ RevShopClient/src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RemoteService } from '../remote.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-cart',
@@ -12,10 +13,15 @@ export class CartComponent {
 
   // only want to load cart from repository when page is loaded for the session, otherwise use sessionStorage
   remoteService: RemoteService;
+  username: string;
 
-  constructor(remoteService: RemoteService) {
+  constructor(remoteService: RemoteService, private cookieService: CookieService) {
     this.remoteService = remoteService;
+
+    // fetch the token stored as a cookie from LoginComponent
+    this.username = this.remoteService.decodeToken(this.cookieService.get('token'));
+    console.log(this.username);
   }
   
-  
 }
+
